b6dd71ca35cf2a16b529985faa83eab98b173f7d
sainan <2698236846@qq.com>
2019-01-08 17:55:26 +0800

    第一次提交
    添加了一些代码，书籍在book中

---
+++ change_case.py
@@ -0,0 +1,18 @@
+name = "add lovelace"
+print(name.title())
+print(name.lower())
+print(name.upper())
+first_name="sun"
+last_name="sainan"
+full_name=first_name+" "+last_name
+print(full_name)
+# full_name=first_name+last_name
+# print(full_name)
+message = "Hello "+full_name.title()+"!"
+print(message)
+print("\tmessage")
+print("Language:\n\tPython\n\tc\n\tc++")
+messa = " pyth on"
+print(messa)
+messa = "python"
+print(messa.rstrip())
---
+++ loop.py
@@ -0,0 +1,6 @@
+magicians = ['alice','david','carolina']
+for magi in magicians:
+    print(magi.title()+",that was a great trick")
+    print("I can't wait to see your next trick,"+magi.title()+".\n")
+    
+    print("Thank you,everyone. That was a great magic show!")
