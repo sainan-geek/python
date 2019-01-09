0fc49c43638b0b257eb2e045f42ec5c33dd20610
sainan <2698236846@qq.com>
2019-01-09 11:19:52 +0800

    2019-1-9日上午
    学习了if else elif

---
+++ dimension.py
@@ -0,0 +1,3 @@
+dimensions = (200,5)
+print(dimensions[0])
+print(dimensions[1])
---
+++ if_else.py
@@ -0,0 +1,15 @@
+age = 17
+if age >= 18:
+    print("do if")
+    print("age>=18")
+else:
+    print("do else")
+    print("age<18")
+
+age = 12
+if age<4:
+    print("You admission cost is $0")
+elif age<18:
+    print("You admission cost is $5")
+else:
+    print("You admission cost is $10")
