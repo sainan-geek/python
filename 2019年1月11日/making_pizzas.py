cd197aa659ebd038f43723f281a6e80edda75542
sainan-geek <sun.sainan2332@gmail.com>
2019-01-11 16:53:14 +0800

    主要内容为导入函数

---
+++ 2019年1月11日/making_pizzas.py
@@ -0,0 +1,7 @@
+import pizza
+from print_li import print_plus as fun
+#from print_li import *
+
+pizza.make_pizza(16,'pepperoni')
+fun()
+pizza.make_pizza(12,'mushrooms','green peppers','extra cheese')
