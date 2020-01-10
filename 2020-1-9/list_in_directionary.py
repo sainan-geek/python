1a77bc11ae4c87aec5c5501ea83a65bb21eca414
sainan-geek <sun.sainan2332@gmail.com>
2020-01-10 09:00:19 +0800

    同步windows

---
+++ 2020-1-9/list_in_directionary.py
@@ -0,0 +1,7 @@
+pizza = {
+    'crust':'thick',
+    'toppings':['mushrooms','extra cheese'],
+}
+print("You ordered a "+pizza['crust']+"-crust pizza"+ "with the following topping:")
+for topping in pizza['toppings']:
+    print("\t"+topping)
