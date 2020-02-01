3cdb2129c649516676f8f63aa716930f113ce686
sainan-geek <sun.sainan2332@gmail.com>
2020-02-01 17:41:35 +0800

    2020-2-1

---
+++ windows/my_pro/hm_05_绘制英雄图像.py
@@ -0,0 +1,23 @@
+import pygame
+
+pygame.init()
+
+# 创建游戏的窗口 480 * 700
+screen = pygame.display.set_mode((480, 700))
+
+# 绘制背景图像
+# > 加载图像数据
+bg = pygame.image.load("./images/background.png") # .当前文件夹下 返回值用bg记录
+# > blit绘制图像
+screen.blit(bg, (0, 0))
+# > update 更新屏幕显示
+pygame.display.update() # 要想在屏幕上显示一定要调用
+
+# 绘制英雄的飞机
+hero = pygame.image.load("./images/me1.png")
+screen.blit(hero, (150, 500))
+pygame.display.update()
+
+while True:
+    pass
+pygame.quit()
